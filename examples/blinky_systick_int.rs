b9b5977c9ebb0ce75e13fd5a2c4bc1d651420489
David Sawatzke <david-sawatzke@users.noreply.github.com>
2019-01-14 13:22:03 +0100

    Add sample code (hack)

---
+++ examples/blinky_systick_int.rs
@@ -0,0 +1,102 @@
+#![no_main]
+#![no_std]
+
+#[allow(unused)]
+use panic_halt;
+
+use xmc1100_hal as hal;
+
+use crate::hal::gpio::*;
+use crate::hal::prelude::*;
+use crate::hal::xmc1100;
+
+use cortex_m::interrupt::Mutex;
+use cortex_m::peripheral::syst::SystClkSource::Core;
+use cortex_m::peripheral::Peripherals;
+use cortex_m_rt::{entry, exception};
+
+use core::cell::RefCell;
+use core::ops::DerefMut;
+use xmc1100_hal::rcc::Clocks;
+use xmc1100_hal::time::MegaHertz;
+
+static PORT: Mutex<RefCell<Option<port1::P1_1<Output<PushPull>>>>> = Mutex::new(RefCell::new(None));
+
+#[entry]
+fn main() -> ! {
+    if let (Some(p), Some(cp)) = (xmc1100::Peripherals::take(), Peripherals::take()) {
+        let port1 = p.PORT1.split();
+
+        /* (Re-)configure PA1 as output */
+        let led = port1.p1_1.into_push_pull_output();
+
+        cortex_m::interrupt::free(move |cs| {
+            let _clocks = Clocks {
+                sysclk: MegaHertz(32).into(),
+            };
+
+            let mut syst = cp.SYST;
+
+            *PORT.borrow(cs).borrow_mut() = Some(led);
+
+            /* Initialise SysTick counter with a defined value */
+            unsafe { syst.cvr.write(1) };
+
+            /* Set source for SysTick counter, here full operating frequency (== 8MHz) */
+            syst.set_clock_source(Core);
+
+            /* Set reload value, i.e. timer delay 32 MHz/4 Mcounts == 8Hz or 125ms */
+            syst.set_reload(4_000_000 - 1);
+
+            /* Start counter */
+            syst.enable_counter();
+
+            /* Start interrupt generation */
+            syst.enable_interrupt();
+        });
+
+        let mut test = port1.p1_0.into_push_pull_output();
+        loop {
+            let x = unsafe { HardFault_Veneer };
+            /* Turn PA1 on a million times in a row */
+            for _ in 0..x {
+                test.set_high();
+            }
+        }
+    }
+    loop {
+        continue;
+    }
+}
+
+/* Define an exception, i.e. function to call when exception occurs. Here if our SysTick timer
+ * trips the flash function will be called and the specified stated passed in via argument */
+//, flash, state: u8 = 1);
+#[exception]
+fn SysTick() -> ! {
+    static mut state: u8 = 1;
+
+    /* Enter critical section */
+    cortex_m::interrupt::free(|cs| {
+        if let Some(ref mut led) = *PORT.borrow(cs).borrow_mut().deref_mut() {
+            /* Check state variable, keep LED off most of the time and turn it on every 10th tick */
+            if *state < 10 {
+                /* If set turn off the LED */
+                led.set_low();
+
+                /* And now increment state variable */
+                *state += 1;
+            } else {
+                /* If not set, turn on the LED */
+                led.set_high();
+
+                /* And set new state variable back to 0 */
+                *state = 0;
+            }
+        }
+    });
+}
+
+extern "C" {
+    static HardFault_Veneer: u32;
+}
