e47bd3963cb1a9589bfd717d0417b78102add10e
David Sawatzke <david-sawatzke@users.noreply.github.com>
2019-03-17 01:38:00 +0100

    Remove leftover comment

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,3 +1,2 @@
-pub use embedded_hal::prelude::*;
-// TODO for some reason, watchdog isn't in the embedded_hal prelude
 pub use crate::gpio::GpioExt as _xmc1100_hal_gpio_GpioExt;
+pub use embedded_hal::prelude::*;
